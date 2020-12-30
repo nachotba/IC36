9af3f4594697c98f6cc880109ad81e1a865baffe
Nacho <nachotba.js@gmail.com>
2020-12-30 15:22:29 -0300

    Ultima actualizacion funcionando

--- app/src/main/java/com/Admin_user/ic36/Inicio.java
+++ app/src/main/java/com/Admin_user/ic36/Inicio.java
@@ -1,6 +1,9 @@
 package com.Admin_user.ic36;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
@@ -10,10 +13,13 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class Inicio extends AppCompatActivity {
+public class Inicio extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     //private Toolbar toolbar;
+
+    private SharedPreferences prefs;
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
@@ -24,6 +30,8 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        prefs = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
 
@@ -47,5 +55,28 @@ public class Inicio extends AppCompatActivity {
     public boolean onCreateOptionsMenu (Menu menu){
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.salir:
+                removeSharedPreference();
+                logOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void logOut() {
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+    private void removeSharedPreference() { prefs.edit().clear().apply(); }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
